25a1b51a13b0bb0d899ea1deaa1b78ee0b9f587e
Harikrishnan <harikrishnankannoth@gmail.com>
2023-01-04 00:39:40 +0530

    Adding Sounds to the buttons

--- index.js
+++ index.js
@@ -1,13 +1,48 @@
+let player = (key)=>{
+    switch(key)
+    {
+        case "w":
+            var audio = new Audio("./sounds/tom-1.mp3"); 
+            break;
+        case "a":
+            var audio = new Audio("./sounds/tom-2.mp3"); 
+            break;  
+        case "s":
+            var audio = new Audio("./sounds/tom-3.mp3"); 
+            break;   
+        case "d":
+            var audio = new Audio("./sounds/tom-4.mp3"); 
+            break;  
+        case "j":
+            var audio = new Audio("./sounds/crash.mp3"); 
+            break;              
+        case "k":
+            var audio = new Audio("./sounds/kick-bass.mp3"); 
+            break;  
+        case "l":
+            var audio = new Audio("./sounds/snare.mp3"); 
+            break;  
 
-let handleClick = ()=>{
-    alert("I got clicked");
+    }
+    audio.play();
 }
+
+
 
 for(let i =0;i<document.querySelectorAll(".drum").length;i++)
 {
-document.querySelectorAll("button")[i].addEventListener("click",()=>{
-    alert("New Alert")
+document.querySelectorAll(".drum")[i].addEventListener("click",(e)=>{
+    
+    var key = e.target.innerHTML;
+    player(key);
+    
 });
 }
+
+document.addEventListener("keypress",(e)=>{
+   console.log(e)
+   let key= e.target.key;
+   player(e.target.key)
+})
 
 
